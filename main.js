6da5b93ad3bf685c09a0a3968a2e3ad7cca96153
StarKaotic <ghostbaby579@gmail.com>
2023-10-23 22:48:55 -0300

    repaso de js

---
+++ main.js
@@ -0,0 +1,132 @@
+//variable var 
+
+//var miVariable = 24;
+
+//console.log("edad es" + miVariable)
+
+//Variable const
+
+//const miConstante = 1;
+//console.log("el valor de mi constante es" + miConstante)
+
+//Variables con sumas 
+
+//var resultado = 2 + 2;
+//console.log("resultado vale" + resultado)
+// con este tipo de variables podemos hacer multipicaciones 
+// dividir 
+// tambien podemos hacer operaciones entre variables 
+
+// Operaciones entre variables 
+
+//var op1 = 2;
+//var op2 = 3;
+//var resultado = op1 + op2; 
+//console.log("resultado vale" + resultado)
+
+//condicionales 
+//usar la variable  let mas moderno
+
+//let miNumero = 6
+//if (miNumero == 6){
+//  console.log(" si ");
+//} else {
+//    console.log("no")
+
+//Bucles
+
+//Bucle (while)
+ 
+//let contador = 0
+//while(contador < 10){
+//    console.log("hola a todos")
+//
+//    contador = contador + 1
+//}
+
+//Bucle (for)
+
+//for (let contador = 0; contador < 10; contador = contador + 1) {
+ //   console.log(contador)
+//}
+
+//tambien puedo identar el valor de los contadores con (i)
+
+//for (let i = 0; i < 10; i = i + 1) {
+//    console.log(i)
+//}
+
+//funcion 
+
+//function saludar(){
+ //   console.log("bienvenidos a esta funcion")
+//}
+
+//asi llamamos a la funcion 
+
+//saludar();
+
+//funciones con argumentos 
+// este tipo de funciones puede valer cualquier valor 
+
+//function saludar(nombre, edad){
+ //   console.log("mi nombre es " + nombre);
+//    console.log("mi edad es " + edad);
+//}
+
+//asi llamamos esta funcion con su argumento
+
+//saludar("Aaron",23);
+
+//funciones multiplicando 
+
+//function multiplicar(numero1, numero2){
+//    let resultado = numero1 + numero2;
+//    return resultado;
+//}
+
+//para  resivirlo  
+
+//console.log(multiplicar(2,8));
+
+//Array 
+
+//let miArray = ["david", "fernando", "kevin", "carla"];
+
+//for (let i = 0; i < 4; i++){
+//console.log("access indice" + i);
+//let mostrar = miArray[i];
+//console.log(mostrar);
+//}
+
+//objetos
+// puedo hacer que mi objeto sea una persona x
+
+//let persona = {
+//    nombre : "David",
+//    edad: 15,
+//    masculino: true
+//}
+
+//let persona2 = {
+//    nombre : "carla",
+//    edad: 15,
+//    femenina: true
+//}
+
+// imprimimos de la siguente manera 
+
+//console.log(persona);
+//console.log(persona2);
+
+// estructura mas desarrolada 
+
+//podemos arreglar funciones de maneras la cual salga la que nosotros queramos 
+//utilizando persona.nombre = "nuevo nombre";
+
+//persona.nombre = "nuevo nombre";
+
+//de la siguiente manera puedo acceder al nombre y mostrar solo en nombre o 
+// mostrar la edad o el sexo de persona x de la siguiente manera
+
+//console.log(persona.masculino)
